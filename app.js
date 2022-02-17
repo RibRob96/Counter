38b2b6101e503a3e4d4332d850fd074b301a6fb7
Rob Harwood <43217009+RibRob96@users.noreply.github.com>
2022-02-17 01:44:32 -0500

    Finished javascript implementation

---
+++ app.js
@@ -0,0 +1,33 @@
+//Set initial count
+let count = 0;
+
+//Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){ //e is a reference to an event
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++;
+        }
+        else if (styles.contains("reset")){
+            count = 0;
+        }
+
+        if(count > 0){
+            value.style.color = "green";
+        }
+        else if(count == 0){
+            value.style.color = "black";
+        }
+        else if (count < 0){
+            value.style.color = "red";
+        }
+        
+        value.textContent = count;
+    })
+});
